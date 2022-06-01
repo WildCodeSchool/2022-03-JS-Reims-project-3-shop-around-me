f895ad4328180f9cf51c348a71a17d937e1f5e04
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-01 11:56:36 +0200

    Create keyword controller

---
+++ backend/src/controllers/KeywordController.js
@@ -0,0 +1,83 @@
+const models = require("../models");
+
+class KeywordController {
+  static browse = (req, res) => {
+    models.keyword
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.keyword
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const keyword = req.body;
+
+    // TODO validations (length, format...)
+
+    keyword.id = parseInt(req.params.id, 10);
+
+    models.keyword
+      .update(keyword)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const keyword = req.body;
+
+    // TODO validations (length, format...)
+
+    models.keyword
+      .insert(keyword)
+      .then(([result]) => {
+        res.status(201).send({ ...keyword, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.keyword
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = KeywordController;
