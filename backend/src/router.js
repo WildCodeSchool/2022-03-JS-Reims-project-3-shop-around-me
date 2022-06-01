1ae60440e11ca857550ae9ca576210c818da3ece
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-01 11:58:28 +0200

    Update routes for keyword

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { ItemController } = require("./controllers");
+const { KeywordController } = require("./controllers");
 
 const router = express.Router();
 
@@ -10,10 +11,10 @@ router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
 
-router.get("/keyword", ItemController.browse);
-router.get("/keyword/:id", ItemController.read);
-router.put("/keyword/:id", ItemController.edit);
-router.post("/keyword", ItemController.add);
-router.delete("/keyword/:id", ItemController.delete);
+router.get("/keyword", KeywordController.browse);
+router.get("/keyword/:id", KeywordController.read);
+router.put("/keyword/:id", KeywordController.edit);
+router.post("/keyword", KeywordController.add);
+router.delete("/keyword/:id", KeywordController.delete);
 
 module.exports = router;
