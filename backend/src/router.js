9369325442f76b2c20ee608d81acff2aefd518f2
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-01 17:21:42 +0200

    Update of the routes after the comments"

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,20 +1,19 @@
 const express = require("express");
 
-const { ItemController } = require("./controllers");
-const { KeywordController } = require("./controllers");
+const { ItemController, KeywordController } = require("./controllers");
 
 const router = express.Router();
 
-router.get("/keyword", ItemController.browse);
+router.get("/items", ItemController.browse);
 router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
 
-router.get("/keyword", KeywordController.browse);
-router.get("/keyword/:id", KeywordController.read);
-router.put("/keyword/:id", KeywordController.edit);
-router.post("/keyword", KeywordController.add);
-router.delete("/keyword/:id", KeywordController.delete);
+router.get("/keywords", KeywordController.browse);
+router.get("/keywords/:id", KeywordController.read);
+router.put("/keywords/:id", KeywordController.edit);
+router.post("/keywords", KeywordController.add);
+router.delete("/keywords/:id", KeywordController.delete);
 
 module.exports = router;
