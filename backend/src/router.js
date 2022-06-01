b3c5ac126bbe0607a152a393f0cd62f9a89ed708
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-01 11:48:49 +0200

    Add keyword routes in router.js

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,16 @@ const { ItemController } = require("./controllers");
 
 const router = express.Router();
 
-router.get("/items", ItemController.browse);
+router.get("/keyword", ItemController.browse);
 router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
+
+router.get("/keyword", ItemController.browse);
+router.get("/keyword/:id", ItemController.read);
+router.put("/keyword/:id", ItemController.edit);
+router.post("/keyword", ItemController.add);
+router.delete("/keyword/:id", ItemController.delete);
 
 module.exports = router;
