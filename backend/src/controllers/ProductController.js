eb91d0ce3f24210c3b837184cf99ec60344c9020
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-01 12:16:22 +0200

    Create ProductController.js

---
+++ backend/src/controllers/ProductController.js
@@ -0,0 +1,83 @@
+const models = require("../models");
+
+class ProductController {
+  static browse = (req, res) => {
+    models.product
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static read = (req, res) => {
+    models.product
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = (req, res) => {
+    const product = req.body;
+
+    // TODO validations (length, format...)
+
+    product.id = parseInt(req.params.id, 10);
+
+    models.product
+      .update(product)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static add = (req, res) => {
+    const product = req.body;
+
+    // TODO validations (length, format...)
+
+    models.product
+      .insert(product)
+      .then(([result]) => {
+        res.status(201).send({ ...product, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static delete = (req, res) => {
+    models.product
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
+
+module.exports = ProductController;
