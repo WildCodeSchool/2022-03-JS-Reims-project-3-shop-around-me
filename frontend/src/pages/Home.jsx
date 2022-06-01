7e93d47ce5139c2939a63209dcfb491dc80252c6
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-01 11:08:56 +0200

    create searchbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <p>Hello World</p>;
+  return (
+    <>
+      <label htmlFor="search">
+        <input
+          id="search"
+          type="text"
+          name="search"
+          placeholder="Recherche ..."
+        />
+      </label>
+      <button type="button">🔎</button>
+    </>
+  );
 }
