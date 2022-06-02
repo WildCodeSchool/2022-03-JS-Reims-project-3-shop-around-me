85a835a36dbcfd516a85079fa4963050aba638b5
Simon Duc <ducsimon@gmail.com>
2022-06-02 16:40:01 +0200

    fetch our product table to display a result related to the search value on submit in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,46 @@
+import { useRef, useState } from "react";
+import axios from "axios";
+
 export default function Home() {
-  return <p>Hello World</p>;
+  const searchValue = useRef();
+
+  const [results, setResults] = useState([]);
+  const getResults = () => {
+    axios
+      .get("http://localhost:5001/products/")
+      .then((response) => response.data)
+      .then((data) => {
+        setResults(data);
+      });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.warn(searchValue.current.value);
+    getResults();
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">
+          <input
+            id="search"
+            type="text"
+            name="search"
+            placeholder="Recherche ..."
+            ref={searchValue}
+          />
+        </label>
+        <button type="submit">🔎</button>
+      </form>
+      <ul>
+        {results
+          .filter((result) => result.name.includes(searchValue.current.value))
+          .map((result) => (
+            <li>{result.name}</li>
+          ))}
+      </ul>
+    </>
+  );
 }
