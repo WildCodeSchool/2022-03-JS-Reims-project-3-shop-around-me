844ae34fe9ab4a0148727feb48ce35ca61403ab7
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-10 09:33:45 +0200

    Add color taupe-100 custom class to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      taupe: {
+        100: "#bcb4ac",
+      },
+    },
   },
   plugins: [],
 };
