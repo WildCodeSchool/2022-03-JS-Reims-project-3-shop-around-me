e4f17ade80e95f5a511b6693ae14e2b8b1bf1165
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-14 22:24:40 +0200

    Add css tailwind

--- frontend/src/components/InscriptionForm.jsx
+++ frontend/src/components/InscriptionForm.jsx
@@ -29,131 +29,228 @@ export default function InscriptionForm() {
       {isSubmitSuccessful && (
         <div className="alert alert-success">Formulaire envoyé</div>
       )}
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="firstName">
-          Prénom
-          <input
-            type="text"
-            placeholder="Jane"
-            {...register("firstName", {
-              required: true,
-              minLength: 3,
-              maxLength: 50,
-              pattern: /^([ \u00c0-\u01ffa-zA-Z'-])+$/i,
-            })}
-          />
-          {errors?.firstName?.type === "required" && (
-            <p>Ce champ est requis.</p>
-          )}
-          {errors?.firstName?.type === "minLength" && (
-            <p>Votre prénom ne peut pas faire moins de 3 caractères.</p>
-          )}
-          {errors?.firstName?.type === "maxLength" && (
-            <p>Votre prénom ne peut pas exceder 50 caractères.</p>
-          )}
-          {errors?.firstName?.type === "pattern" && (
-            <p>Ce champ n'accèpte pas la saisie de caractères spéciaux.</p>
-          )}
-        </label>
-        <label htmlFor="lastName">
-          Nom
-          <input
-            type="text"
-            placeholder="Doe"
-            {...register("lastNname", {
-              required: true,
-              minLength: 3,
-              maxLength: 80,
-              pattern: /^([ \u00c0-\u01ffa-zA-Z'-])+$/i,
-            })}
-          />
-          {errors?.lastName?.type === "required" && <p>Ce champ est requis.</p>}
-          {errors?.lastName?.type === "minLength" && (
-            <p>Votre nom ne peut pas faire moins de 3 caractères.</p>
-          )}
-          {errors?.lastName?.type === "maxLength" && (
-            <p>Votre nom ne peut pas exceder 80 caractères.</p>
-          )}
-          {errors?.lastName?.type === "pattern" && (
-            <p>Ce champ n'accèpte pas la saisie de caractères spéciaux.</p>
-          )}
-        </label>
-        <label htmlFor="birthDate">
-          Date de naissance
-          <input
-            type="date"
-            placeholder="01/01/2000"
-            {...register("birthDate", {
-              require: true,
-              pattern:
-                /\s+(?:0[1-9]|[12][0-9]|3[01])[-/.](?:0[1-9]|1[012])[-/.](?:19\d{2}|20[01][0-9]|2020)\b/,
-            })}
-          />
-          {errors?.birthdate?.type === "required" && (
-            <p>Ce champ est requis.</p>
-          )}
-          {errors?.birthdate?.type === "pattern" && (
-            <p>Merci de respecter le format suivant : "jj/mm/aaaa".</p>
-          )}
-        </label>
-        <label htmlFor="gender">
-          Genre
-          <select name="gender" {...register("gender", { required: true })}>
-            <option value="female">Femme</option>
-            <option value="male">Homme</option>
-            <option value="non-binary">Non-binaire</option>
-            <option value="other">Autre</option>
-            <option value="not-tell">Ne se prononce pas</option>
-          </select>
-          {errors?.gender?.type === "required" && <p>Ce champ est requis.</p>}
-        </label>
-        <label htmlFor="city">
-          Ville
-          <input
-            type="text"
-            placeholder="Paris"
-            {...register({
-              required: false,
-              minLength: 3,
-              maxLength: 80,
-              pattern: /^([ \u00c0-\u01ffa-zA-Z'-])+$/i,
-            })}
-          />
-          {errors?.city?.type === "minLength" && (
-            <p>
-              Le nom de votre ville ne peut pas faire moins de 3 caractères.
-            </p>
-          )}
-          {errors?.city?.type === "maxLength" && (
-            <p>Le nom de votre ville ne peut pas exceder 80 caractères.</p>
-          )}
-          {errors?.city?.type === "pattern" && (
-            <p>Ce champs n'accepte pas les caractères spéciaux.</p>
-          )}
-        </label>
-        <label htmlFor="zipcode">
-          Code postal
-          <input
-            type="text"
-            placeholder="75020"
-            {...register("zipcode", {
-              required: false,
-              minLength: 4,
-              maxLength: 8,
-              pattern: /^[0-9]{4}$/,
-            })}
-          />
-          {errors?.zipcode?.type === "minLength" && (
-            <p>Votre code postal ne peut pas faire moins de 4 caractères.</p>
-          )}
-          {errors?.zipcode?.type === "maxLength" && (
-            <p>Votre code postal ne peut pas faire plus de 8 caractères.</p>
-          )}
-          {errors?.zipcode?.type === "pattern" && (
-            <p>Ce champs n'accepte pas les caractères spéciaux.</p>
-          )}
-        </label>
-        <label htmlFor="email">
+      <form className="w-full max-w-lg" onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-firstName"
+            >
+              Prénom
+              <input
+                type="text"
+                placeholder="Jane"
+                {...register("firstName", {
+                  required: true,
+                  minLength: 3,
+                  maxLength: 50,
+                  pattern: /^([ \u00c0-\u01ffa-zA-Z'-])+$/i,
+                })}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-firstName"
+              />
+              {errors?.firstName?.type === "required" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champ est requis.
+                </p>
+              )}
+              {errors?.firstName?.type === "minLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Votre prénom ne peut pas faire moins de 3 caractères.
+                </p>
+              )}
+              {errors?.firstName?.type === "maxLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Votre prénom ne peut pas exceder 50 caractères.
+                </p>
+              )}
+              {errors?.firstName?.type === "pattern" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champ n'accèpte pas la saisie de caractères spéciaux.
+                </p>
+              )}
+            </label>
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-lastName"
+            >
+              Nom
+              <input
+                type="text"
+                placeholder="Doe"
+                {...register("lastNname", {
+                  required: true,
+                  minLength: 3,
+                  maxLength: 80,
+                  pattern: /^([ \u00c0-\u01ffa-zA-Z'-])+$/i,
+                })}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-lastName"
+              />
+              {errors?.lastName?.type === "required" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champ est requis.
+                </p>
+              )}
+              {errors?.lastName?.type === "minLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Votre nom ne peut pas faire moins de 3 caractères.
+                </p>
+              )}
+              {errors?.lastName?.type === "maxLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Votre nom ne peut pas exceder 80 caractères.
+                </p>
+              )}
+              {errors?.lastName?.type === "pattern" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champ n'accèpte pas la saisie de caractères spéciaux.
+                </p>
+              )}
+            </label>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-birthDate"
+            >
+              Date de naissance
+              <input
+                type="date"
+                placeholder="01/01/2000"
+                {...register("birthDate", {
+                  require: true,
+                  pattern:
+                    /\s+(?:0[1-9]|[12][0-9]|3[01])[-/.](?:0[1-9]|1[012])[-/.](?:19\d{2}|20[01][0-9]|2020)\b/,
+                })}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-birthDate"
+              />
+              {errors?.birthdate?.type === "required" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champ est requis.
+                </p>
+              )}
+              {errors?.birthdate?.type === "pattern" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Merci de respecter le format suivant : "jj/mm/aaaa".
+                </p>
+              )}
+            </label>
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+              htmlFor="grid-gender"
+            >
+              Genre
+              <div className="relative">
+                <select
+                  name="gender"
+                  {...register("gender", { required: true })}
+                  className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                >
+                  <option value="female">Femme</option>
+                  <option value="male">Homme</option>
+                  <option value="non-binary">Non-binaire</option>
+                  <option value="other">Autre</option>
+                  <option value="not-tell">Ne se prononce pas</option>
+                </select>
+                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </div>
+              </div>
+              {errors?.gender?.type === "required" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champ est requis.
+                </p>
+              )}
+            </label>
+          </div>
+        </div>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              htmlFor="grid-city"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            >
+              Ville
+              <input
+                type="text"
+                placeholder="Paris"
+                {...register({
+                  required: false,
+                  minLength: 3,
+                  maxLength: 80,
+                  pattern: /^([ \u00c0-\u01ffa-zA-Z'-])+$/i,
+                })}
+              />
+              {errors?.city?.type === "minLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Le nom de votre ville ne peut pas faire moins de 3 caractères.
+                </p>
+              )}
+              {errors?.city?.type === "maxLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Le nom de votre ville ne peut pas exceder 80 caractères.
+                </p>
+              )}
+              {errors?.city?.type === "pattern" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champs n'accepte pas les caractères spéciaux.
+                </p>
+              )}
+            </label>
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <label
+              htmlFor="grid-zipcode"
+              className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+            >
+              Code postal
+              <input
+                type="text"
+                placeholder="75020"
+                {...register("zipcode", {
+                  required: false,
+                  minLength: 4,
+                  maxLength: 8,
+                  pattern: /^[0-9]{4}$/,
+                })}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              />
+              {errors?.zipcode?.type === "minLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Votre code postal ne peut pas faire moins de 4 caractères.
+                </p>
+              )}
+              {errors?.zipcode?.type === "maxLength" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Votre code postal ne peut pas faire plus de 8 caractères.
+                </p>
+              )}
+              {errors?.zipcode?.type === "pattern" && (
+                <p className="text-red-600 normal-case text-xs font-normal italic">
+                  Ce champs n'accepte pas les caractères spéciaux.
+                </p>
+              )}
+            </label>
+          </div>
+        </div>
+        <label
+          htmlFor="email"
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        >
           Email
           <input
             type="text"
@@ -164,19 +261,29 @@ export default function InscriptionForm() {
               maxLength: 100,
               pattern: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,5}/i,
             })}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           />
           {errors?.email?.type === "required" && <p>Ce champ est requis.</p>}
           {errors?.email?.type === "minLength" && (
-            <p>Votre adresse mail est trop courte.</p>
+            <p className="text-red-600 normal-case text-xs font-normal italic">
+              Votre adresse mail est trop courte.
+            </p>
           )}
           {errors?.email?.type === "maxLength" && (
-            <p>Votre adresse mail est trop longue.</p>
+            <p className="text-red-600 normal-case text-xs font-normal italic">
+              Votre adresse mail est trop longue.
+            </p>
           )}
           {errors?.email?.type === "pattern" && (
-            <p>Votre adresse mail n'est pas valide.</p>
+            <p className="text-red-600 normal-case text-xs font-normal italic">
+              Votre adresse mail n'est pas valide.
+            </p>
           )}
         </label>
-        <label htmlFor="password">
+        <label
+          htmlFor="password"
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        >
           Mot de passe
           <input
             type="password"
@@ -187,16 +294,23 @@ export default function InscriptionForm() {
               pattern:
                 /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
             })}
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
           />
           {errors?.password?.type === "required" && <p>Ce champ est requis.</p>}
           {errors?.password?.type === "minLength" && (
-            <p>Votre mot de passe est trop court.</p>
+            <p className="text-red-600 normal-case text-xs font-normal italic">
+              Votre mot de passe est trop court.
+            </p>
           )}
           {errors?.password?.type === "maxLength" && (
-            <p>Votre mot de passe est trop long.</p>
+            <p className="text-red-600 normal-case text-xs font-normal italic">
+              Votre mot de passe est trop long.
+            </p>
           )}
           {errors?.password?.type === "pattern" && (
-            <p>Votre mot de passe n'est pas valide.</p>
+            <p className="text-red-600 normal-case text-xs font-normal italic">
+              Votre mot de passe n'est pas valide.
+            </p>
           )}
         </label>
         <input type="submit" disabled={isSubmitting} />
