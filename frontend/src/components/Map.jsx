b1b80401e2d32b4234b890e5dc272dae63c08403
Simon Duc <ducsimon@gmail.com>
2022-06-15 11:39:48 +0200

    use geolocation hook to display the user geolocalized marker on the map in Map.jsx

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -2,34 +2,37 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import useGeolocation from "react-hook-geolocation";
 import "leaflet/dist/leaflet.css";
 
-const position = [49.258329, 4.031696];
-const geoposition = [49.26839, 4.0512];
+const shopPosition = [49.258329, 4.031696];
 
 function Map() {
-  const geolocation = useGeolocation();
-
-  geoposition[0] = geolocation.latitude ?? geoposition[0];
-  geoposition[1] = geolocation.longitude ?? geoposition[1];
+  const userGeolocation = useGeolocation();
+  const userPosition = [userGeolocation.latitude, userGeolocation.longitude];
 
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      scrollWheelZoom={false}
-      style={{ height: "300px", width: "80%" }}
-      className="rounded-xl border-solid border border-gray-200"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={position}>
-        <Popup>The shop</Popup>
-      </Marker>
-      <Marker position={geoposition}>
-        <Popup>Your position.</Popup>
-      </Marker>
-    </MapContainer>
+    <div className="flex flex-col text-center">
+      <h1>Résultats :</h1>
+      {!userGeolocation.error && userGeolocation.latitude && (
+        <MapContainer
+          zoom={13}
+          center={userPosition}
+          scrollWheelZoom={false}
+          style={{ height: "300px", width: "800px" }}
+          className="rounded-xl border-solid border border-gray-200"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={shopPosition}>
+            <Popup>The shop</Popup>
+          </Marker>
+
+          <Marker position={userPosition}>
+            <Popup>Your position.</Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </div>
   );
 }
 
