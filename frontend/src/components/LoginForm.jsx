c39a147c5aa527f5b9a0a980119a7f51d498c100
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-15 15:08:11 +0200

    Add style tailwind css

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -4,18 +4,35 @@ export default function LoginForm() {
   const { handleSubmit } = useForm();
 
   const onSubmit = (data) => {
-    console.warn(JSON.stringify(data, null, 2));
+    console.warn(JSON.stringify(data));
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="email">
-        Email <input type="text" />
-      </label>
-      <label htmlFor="password">
-        Mot de passe <input type="password" />
-      </label>
-      <input type="submit" />
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label
+          htmlFor="email"
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        >
+          Email
+          <input
+            type="text"
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </label>
+        <label
+          htmlFor="password"
+          className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+        >
+          Mot de passe
+          <input
+            type="password"
+            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          />
+        </label>
+        <input type="submit" />
+      </form>
+      <span>Vous n’avez pas de compte ? Inscrivez-vous</span>
+    </>
   );
 }
