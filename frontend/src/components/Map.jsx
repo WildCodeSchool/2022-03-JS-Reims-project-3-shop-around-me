5c3c12eb90a8b0c5323110a97e7fba4579f7a46b
Simon Duc <ducsimon@gmail.com>
2022-06-15 12:43:10 +0200

    modify the map component to display a map even if the user position is not known

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -4,34 +4,41 @@ import "leaflet/dist/leaflet.css";
 
 const shopPosition = [49.258329, 4.031696];
 
+const position = (geolocation) => {
+  if (geolocation.error || !geolocation.latitude || !geolocation.longitude) {
+    return null;
+  }
+
+  return [geolocation.latitude, geolocation.longitude];
+};
+
 function Map() {
   const userGeolocation = useGeolocation();
-  const userPosition = [userGeolocation.latitude, userGeolocation.longitude];
+  const userPosition = position(userGeolocation);
 
   return (
     <div className="flex flex-col text-center">
       <h1>Résultats :</h1>
-      {!userGeolocation.error && userGeolocation.latitude && (
-        <MapContainer
-          zoom={13}
-          center={userPosition}
-          scrollWheelZoom={false}
-          style={{ height: "300px", width: "800px" }}
-          className="rounded-xl border-solid border border-gray-200"
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={shopPosition}>
-            <Popup>The shop</Popup>
-          </Marker>
-
+      <MapContainer
+        zoom={13}
+        center={userPosition ?? shopPosition}
+        scrollWheelZoom={false}
+        style={{ height: "300px", width: "800px" }}
+        className="rounded-xl border-solid border border-gray-200"
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={shopPosition}>
+          <Popup>The shop</Popup>
+        </Marker>
+        {!userGeolocation.error && userGeolocation.latitude && (
           <Marker position={userPosition}>
             <Popup>Your position.</Popup>
           </Marker>
-        </MapContainer>
-      )}
+        )}
+      </MapContainer>
     </div>
   );
 }
