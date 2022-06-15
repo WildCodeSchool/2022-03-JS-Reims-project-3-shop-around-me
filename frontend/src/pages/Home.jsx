6eb0760042bcaaaa8710ff4076bd807260f6f06f
Simon Duc <ducsimon@gmail.com>
2022-06-15 10:24:48 +0200

    install react-leaflet and react-hook-geolocation dependencies, create the Map component imported in Home.jsx to display a map

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import SearchBar from "../components/SearchBar";
+import Map from "../components/Map";
 
 export default function Home() {
-  return <SearchBar />;
+  return (
+    <>
+      <SearchBar />
+      <div className="flex justify-center m-4">
+        <Map />
+      </div>
+    </>
+  );
 }
