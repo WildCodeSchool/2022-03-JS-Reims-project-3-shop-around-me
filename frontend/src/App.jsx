ee5e1ced035f36f212e664fb2833a4fb48fc761a
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-16 11:17:01 +0200

    create first route and import react-router-dom in file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import Home from "@pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </Router>
       <Home />
     </div>
   );
