9ceb50fdd85ad315ba2d5a9794d533b2d2d711b6
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-22 10:31:49 +0200

    add some style to the file

--- frontend/src/pages/MyAccount.jsx
+++ frontend/src/pages/MyAccount.jsx
@@ -1,14 +1,57 @@
 export default function MyAccount() {
   return (
-    <>
-      <h2>Mon compte</h2>
+    <div className="flex flex-col gap-y-6 w-screen bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <h2 className="text-">Mon compte</h2>
       <ul>
-        Résumé
-        <li>Historique cagnotte</li>
-        <li>Données personnelles</li>
-        <li>Paramètres de l'application</li>
-        <li>Aide et contact</li>
+        <li className="flex justify-between">
+          Historique cagnotte
+          <div className="pointer-events-none px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 512"
+            >
+              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+            </svg>
+          </div>
+        </li>
+        <li className="flex justify-between">
+          Données personnelles
+          <div className="pointer-events-none px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 512"
+            >
+              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+            </svg>
+          </div>
+        </li>
+        <li className="flex justify-between">
+          Paramètres de l'application
+          <div className="pointer-events-none px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 512"
+            >
+              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+            </svg>
+          </div>
+        </li>
+        <li className="flex justify-between">
+          Aide et contact
+          <div className="pointer-events-none px-2 text-gray-700">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 256 512"
+            >
+              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+            </svg>
+          </div>
+        </li>
       </ul>
-    </>
+    </div>
   );
 }
