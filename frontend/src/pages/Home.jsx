ca35e601fec5ccbff7d02e7bbcc7b7066f659cf6
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-22 14:31:19 +0200

    remove Fav in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,5 @@
 import SearchBar from "../components/SearchBar";
-import Fav from "../components/Fav";
 
 export default function Home() {
-  return (
-    <>
-      <SearchBar />
-      <Fav />
-    </>
-  );
+  return <SearchBar />;
 }
