64947213216d7d5d45547b59df72827fd6c1da3d
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-22 14:09:05 +0200

    Delete a div and add style in the svg className

--- frontend/src/pages/MyAccount.jsx
+++ frontend/src/pages/MyAccount.jsx
@@ -5,63 +5,53 @@ export default function MyAccount() {
       <ul>
         <li className="flex justify-between">
           Historique cagnotte
-          <div className="pointer-events-none px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 512"
-            >
-              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-            </svg>
-          </div>
+          <svg
+            className="fill-current h-4 w-4 text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 256 512"
+          >
+            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+          </svg>
         </li>
         <li className="flex justify-between">
           Données personnelles
-          <div className="pointer-events-none px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 512"
-            >
-              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-            </svg>
-          </div>
+          <svg
+            className="fill-current h-4 w-4 text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 256 512"
+          >
+            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+          </svg>
         </li>
         <li className="flex justify-between">
           Paramètres de l'application
-          <div className="pointer-events-none px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 512"
-            >
-              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-            </svg>
-          </div>
+          <svg
+            className="fill-current h-4 w-4 text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 256 512"
+          >
+            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+          </svg>
         </li>
         <li className="flex justify-between">
           Aide et contact
-          <div className="pointer-events-none px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 512"
-            >
-              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-            </svg>
-          </div>
+          <svg
+            className="fill-current h-4 w-4 text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 256 512"
+          >
+            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+          </svg>
         </li>
         <li className="flex justify-between">
           Supprimer des données
-          <div className="pointer-events-none px-2 text-gray-700">
-            <svg
-              className="fill-current h-4 w-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 512"
-            >
-              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-            </svg>
-          </div>
+          <svg
+            className="fill-current h-4 w-4 text-gray-700"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 256 512"
+          >
+            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
+          </svg>
         </li>
       </ul>
       <button type="button">Se déconnecter</button>
