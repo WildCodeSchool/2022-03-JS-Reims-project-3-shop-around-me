516863aab1f5d507831cd5b9a68e5ab8c99e6554
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-22 16:42:42 +0200

    create object shopExemple

---
+++ frontend/src/components/ShopDetails.jsx
@@ -0,0 +1,21 @@
+export default function ShopDetails() {
+  const shopExemple = {
+    name: "Magasin Fossier",
+    address: "25 Cr Jean-Baptiste Langlet, 51100 Reims",
+    opening_hours: {
+      lundi: "14:00-19:00",
+      mardi: "14:00-19:00",
+      mercredi: "14:00-19:00",
+      jeudi: "14:00-19:00",
+      vendredi: "14:00-19:00",
+      samedi: "14:00-19:00",
+      dimanche: "14:00-19:00",
+    },
+    website: "http://www.fossier.fr/",
+    fb_page: "https://www.facebook.com/biscuitsfossier",
+    insta_page: "https://www.instagram.com/biscuitsfossier/",
+    phone: "03 26 47 59 84",
+  };
+
+  return shopExemple.name;
+}
