4bc378d3bab376a25cf505ab9426e91913801ab3
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-22 11:00:44 +0200

    Delete the MyAccount component from the Home to clean the space

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,5 @@
 import SearchBar from "../components/SearchBar";
-import MyAccount from "./MyAccount";
 
 export default function Home() {
-  return (
-    <>
-      <SearchBar />
-      <MyAccount />
-    </>
-  );
+  return <SearchBar />;
 }
