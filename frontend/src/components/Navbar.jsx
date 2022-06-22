1442f82250f7db79543f4b28623b9d7d3b5669c6
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-22 10:52:47 +0200

    move navbar component to home.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faStore } from "@fortawesome/free-solid-svg-icons";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { faEuroSign } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faHeart,
+  faStore,
+  faHouse,
+  faEuroSign,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   const navBarMenu = [faHouse, faStore, faEuroSign, faHeart, faUser];
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -2,7 +2,6 @@ import { useRef, useState } from "react";
 import axios from "axios";
 import logo from "../assets/images/logo.png";
 import Map from "./Map";
-import Navbar from "./Navbar";
 
 export default function SearchBar() {
   const searchValue = useRef();
@@ -62,7 +61,6 @@ export default function SearchBar() {
           </li>
         ))}
       </ul>
-      <Navbar />
     </div>
   );
 }
