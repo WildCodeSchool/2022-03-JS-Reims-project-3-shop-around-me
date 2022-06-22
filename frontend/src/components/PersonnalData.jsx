8f2d9d45ce7a88b413bd3dc10e6ba668fab482ae
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-22 10:42:07 +0200

    Create PersonnalData.jsx

---
+++ frontend/src/components/PersonnalData.jsx
@@ -0,0 +1,9 @@
+export default function PersonnalData() {
+  return (
+    <div>
+      Votre nom et prénom
+      <button>Modifier mes informations</button>
+      <button>Valider</button>
+    </div>
+  );
+}
