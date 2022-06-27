2fb5331b04b44971bb548a12153c49c992d19f9e
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-27 17:16:09 +0200

    correct css fund page

--- frontend/src/components/Fund.jsx
+++ frontend/src/components/Fund.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 function Fund() {
   return (
-    <main className="text-center h-96 flex flex-col items-center justify-end">
+    <main className="text-center min-h-[92vh] flex flex-col items-center justify-evenly">
       <h1 className="text-4xl">Ma cagnotte</h1>
-      <figure className="flex-col">
+      <picture className="flex-col">
         <img
           src="https://chart.googleapis.com/chart?cht=qr&chl=Shop%20Around%20Me&chs=180x180&choe=UTF-8&chld=L|2%27%20rel=%27nofollow"
           alt="qr code"
         />
-        <figcaption className="text-4xl">50 euros</figcaption>
-      </figure>
+      </picture>
+      <p className="text-4xl">52,56€</p>
     </main>
   );
 }
