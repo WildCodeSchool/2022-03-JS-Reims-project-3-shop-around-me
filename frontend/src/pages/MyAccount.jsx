c85831d10b830123f66d68167c354e3211de5e74
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-28 11:04:38 +0200

    Changes in the structure, dealing with the account menu with a map to have less code

--- frontend/src/pages/MyAccount.jsx
+++ frontend/src/pages/MyAccount.jsx
@@ -9,68 +9,62 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function MyAccount() {
+  const accountMenu = [
+    {
+      id: 1,
+      icon: faPiggyBank,
+      title: "Historique cagnotte",
+      url: "/myaccount/",
+    },
+    {
+      id: 2,
+      icon: faUser,
+      title: "Mes données personnelles",
+      url: "/myaccount/",
+    },
+    {
+      id: 3,
+      icon: faGears,
+      title: "Paramètres de l'application",
+      url: "/myaccount/",
+    },
+    {
+      id: 4,
+      icon: faCircleInfo,
+      title: "Aide et contact",
+      url: "/myaccount/",
+    },
+    {
+      id: 5,
+      icon: faUserXmark,
+      title: "Supprimer mes données",
+      url: "/myaccount/",
+    },
+  ];
   return (
-    <section className="flex flex-col gap-y-6 w-screen bg-white shadow-md rounded px-8 pt-6 pb-8">
-      <h2 className="text-lg">Mon compte</h2>
+    <section className="flex flex-col gap-y-8 w-screen px-8 pt-8 pb-8">
+      <h2 className=" text-4xl text-center">Mon compte</h2>
       <ul>
-        <li className="flex justify-between">
-          <FontAwesomeIcon icon={faPiggyBank} /> Historique cagnotte
-          <svg
-            className="fill-current h-4 w-4 text-gray-700"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 256 512"
+        {accountMenu.map((menu) => (
+          <li
+            key={menu.id}
+            className="mr-4 text-gray-900 text-xl list-none mb-4"
           >
-            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-          </svg>
-        </li>
-        <li>
-          <Link to="/mydata" className="flex justify-between">
-            <FontAwesomeIcon icon={faUser} />
-            Données personnelles
-            <svg
-              className="fill-current h-4 w-4 text-gray-700"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 256 512"
-            >
-              <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-            </svg>
-          </Link>
-        </li>
-        <li className="flex justify-between">
-          <FontAwesomeIcon icon={faGears} />
-          Paramètres de l'application
-          <svg
-            className="fill-current h-4 w-4 text-gray-700"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 256 512"
-          >
-            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-          </svg>
-        </li>
-        <li className="flex justify-between">
-          <FontAwesomeIcon icon={faCircleInfo} />
-          Aide et contact
-          <svg
-            className="fill-current h-4 w-4 text-gray-700"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 256 512"
-          >
-            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-          </svg>
-        </li>
-        <li className="flex justify-between">
-          <FontAwesomeIcon icon={faUserXmark} />
-          Supprimer des données
-          <svg
-            className="fill-current h-4 w-4 text-gray-700"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 256 512"
-          >
-            <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
-          </svg>
-        </li>
+            <Link to={menu.url} className="flex gap-8">
+              <div className="w-[24px] h-[24px]">
+                <FontAwesomeIcon icon={menu.icon} />
+              </div>
+              {menu.title}
+            </Link>
+          </li>
+        ))}
       </ul>
-      <button type="button">Se déconnecter</button>
+      <button
+        type="button"
+        className="text-m py-3 border-solid border-2 border-red-600 text-red-600 font-bold rounded focus:outline-none focus:shadow-outline"
+      >
+        Se déconnecter
+      </button>
     </section>
   );
 }
