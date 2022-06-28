c1c23cc76e7a4e54db059f184c39c5e9b9f131f4
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-06-28 12:06:47 +0200

    Changes of structure, add some margin and space to let the interface breathe

--- frontend/src/pages/MyAccount.jsx
+++ frontend/src/pages/MyAccount.jsx
@@ -7,6 +7,7 @@ import {
   faCircleInfo,
   faUserXmark,
 } from "@fortawesome/free-solid-svg-icons";
+import logo from "../assets/images/logo_alone.png";
 
 export default function MyAccount() {
   const accountMenu = [
@@ -42,9 +43,11 @@ export default function MyAccount() {
     },
   ];
   return (
-    <section className="flex flex-col gap-y-8 w-screen px-8 pt-8 pb-8">
+    <section className="flex flex-col w-screen px-8 pt-8 pb-8 tracking-wide">
+      <img src={logo} alt="logo" className="max-w-[4rem] mr-2 mb-8" />
       <h2 className=" text-3xl text-[#4F4E47]">Bonjour utilisateur !</h2>
-      <h3 className=" text-2xl text-[#4F4E47] -mt-6">Votre compte.</h3>
+      <h3 className=" text-2xl text-[#4F4E47] mb-8">Votre compte.</h3>
+
       <ul>
         {accountMenu.map((menu) => (
           <li
@@ -62,7 +65,7 @@ export default function MyAccount() {
       </ul>
       <button
         type="button"
-        className="text-m py-3 border-solid border-2 border-red-600 text-red-600 font-bold rounded focus:outline-none focus:shadow-outline"
+        className="text-m mt-8 py-3 border-solid border-2 border-red-600 text-red-600 font-bold rounded focus:outline-none focus:shadow-outline"
       >
         Se déconnecter
       </button>
