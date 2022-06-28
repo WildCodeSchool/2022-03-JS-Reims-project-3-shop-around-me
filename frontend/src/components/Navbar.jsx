6d7dc2ef25c1eca765f1409aadc10ac5c532fb3f
MORETTE Valentin <morette.valentin@gmail.com>
2022-06-28 12:27:38 +0200

    order all route with and without navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUser,
@@ -9,16 +9,18 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+  const location = useLocation();
+  const noNavbarPaths = ["/", "/inscription"];
   const navBarMenu = [
     {
       id: 1,
       icon: faHouse,
-      url: "/",
+      url: "/home",
     },
     {
       id: 2,
       icon: faStore,
-      url: "/account",
+      url: "",
     },
     {
       id: 3,
@@ -33,10 +35,10 @@ function Navbar() {
     {
       id: 5,
       icon: faUser,
-      url: "/inscription",
+      url: "/account",
     },
   ];
-  return (
+  return noNavbarPaths.includes(location.pathname) ? null : (
     <nav className="fixed h-[8vh] w-screen bottom-0 bg-[#4F4E47]">
       <ul className="flex items-center justify-evenly h-[8vh]">
         {navBarMenu.map((menu) => (
