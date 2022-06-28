aa2af195071fea9144efb3f4d13da41b4d775623
Simon Duc <ducsimon@gmail.com>
2022-06-28 11:17:28 +0200

    remove useless div tags and tailwind css classes, replace the logo, replace the section tags to article tags

--- frontend/src/components/RetailerArea.jsx
+++ frontend/src/components/RetailerArea.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import logo from "../assets/images/logo.png";
+import Logo from "./Logo";
 
 const retailerFormFields = [
   {
@@ -74,17 +74,17 @@ export default function RetailerArea() {
   const [search, setSearch] = useState("");
 
   return (
-    <div className="grid place-items-center block uppercase tracking-wide text-[#4F4E47] mb-2">
-      <header>
-        <img src={logo} alt="logo" className="max-w-xs m-4" />
-      </header>
-      <main className="flex flex-col justify-center w-4/5">
-        <h1 className="text-xl font-bold mb-8 mt-4 text-center">
+    <main className="grid place-items-center block uppercase tracking-wide text-[#4F4E47] mb-2">
+      <picture>
+        <Logo />
+      </picture>
+      <section className="flex flex-col justify-center w-4/5">
+        <h1 className="text-xl font-bold tracking-wide mb-6 mt-4 text-center">
           Espace commerçant
         </h1>
-        <section className="border-t-2 border-[#4F4E47]">
+        <article className="border-t-2 border-[#4F4E47]">
           <h2 className="font-bold mb-2">Voir ma boutique : </h2>
-          <p className="flex justify-between text-xs mb-2">
+          <a href="/#" className="flex justify-between text-sm mb-2">
             Magasin Fossier Reims Cathédrale
             <svg
               className="fill-current h-4 w-4"
@@ -93,65 +93,57 @@ export default function RetailerArea() {
             >
               <path d="M64 448c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L178.8 256L41.38 118.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160c12.5 12.5 12.5 32.75 0 45.25l-160 160C80.38 444.9 72.19 448 64 448z" />
             </svg>
-          </p>
-        </section>
-        <section className="mt-2 border-t-2 border-[#4F4E47]">
-          <h2 className="text-s font-bold mb-2">Mes informations :</h2>
-          <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3">
-              <div className="w-full px-3 md:mb-0">
-                {retailerFormFields.map((field) => (
-                  <label
-                    htmlFor={field.title}
-                    className="text-xs mb-2"
-                    key={field.id}
-                  >
-                    {field.title}
-                    <input
-                      id={field.title}
-                      type="text"
-                      className="appearance-none block w-full bg-gray-200 border border-[#4F4E47] rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    />
-                  </label>
-                ))}
-              </div>
-            </div>
-            <input
+          </a>
+        </article>
+        <article className="mt-2 border-t-2 border-[#4F4E47]">
+          <h2 className="font-bold mb-2">Mes informations :</h2>
+          <form className="w-full">
+            {retailerFormFields.map((field) => (
+              <label htmlFor={field.title} className="text-sm" key={field.id}>
+                {field.title}
+                <input
+                  id={field.title}
+                  type="text"
+                  className="appearance-none block w-full bg-gray-200 border border-[#4F4E47] rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 mb-1"
+                />
+              </label>
+            ))}
+            <button
               type="submit"
-              className="block uppercase tracking-wide text-xs"
-            />
+              className="block uppercase tracking-wide text-sm mt-2 underline underline-offset-1"
+            >
+              Envoyer
+            </button>
           </form>
           <button
             type="button"
-            className="block uppercase tracking-wide text-xs mb-2"
+            className="block uppercase tracking-wide text-sm my-1 underline underline-offset-1"
           >
             Modifier les informations
           </button>
-        </section>
-        <section className="mt-2 border-t-2 border-[#4F4E47] text-s mb-4">
+        </article>
+        <article className="mt-2 border-t-2 border-[#4F4E47] text-s mb-4">
           <h2 className="font-bold mb-2">Mes produits : </h2>
           <ul className="mb-2">
             {retailerRecordedProducts.map((product) => (
-              <li key={product.id} className="text-xs">
+              <li key={product.id} className="text-sm mb-1 ml-3 list-disc">
                 {product.name} ✖️
               </li>
             ))}
           </ul>
-          <div className="flex justify-left">
-            <label htmlFor="productSearch" className="w-full text-xs mb-2">
-              Ajouter des produits :
-              <input
-                id="productSearch"
-                type="text"
-                placeholder="Votre recherche"
-                value={search}
-                onChange={(event) => setSearch(event.target.value)}
-                className="appearance-none block w-full bg-gray-200 border border-[#4F4E47] rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              />
-            </label>
-          </div>
 
-          <ul className="mb-12">
+          <label htmlFor="productSearch" className="w-full text-sm">
+            Ajouter des produits :
+            <input
+              id="productSearch"
+              type="text"
+              placeholder="Votre recherche"
+              value={search}
+              onChange={(event) => setSearch(event.target.value)}
+              className="appearance-none block w-full bg-gray-200 border border-[#4F4E47] rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            />
+          </label>
+          <ul className="mt-2 mb-12">
             {productsList
               .filter(
                 (product) => search !== "" && product.name.includes(search)
@@ -159,14 +151,14 @@ export default function RetailerArea() {
               .map((product) => (
                 <li
                   key={product.id}
-                  className="text-xs overflow-hidden overflow-y-auto self-start"
+                  className="text-sm overflow-hidden overflow-y-auto self-start mb-1"
                 >
                   {product.name} ➕
                 </li>
               ))}
           </ul>
-        </section>
-      </main>
-    </div>
+        </article>
+      </section>
+    </main>
   );
 }
