c7c469e9e3aad00fc835c1c2be999d8feb4b25e9
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-07-05 16:05:28 +0200

    Remove user password in GeneratePassword middleware

--- backend/src/services/GenerateToken.js
+++ backend/src/services/GenerateToken.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const GenerateToken = (req, res) => {
-  const token = jwt.sign(req.user, "supersecret");
+  const token = jwt.sign(req.user, process.env.JWT_SECRET);
+  delete req.user.password;
   res.status(200).json({ user: req.user, token });
 };
 
