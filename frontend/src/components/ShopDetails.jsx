c4aff993398c4c13fef13cda74fcf1eb40aa1feb
Simon Duc <ducsimon@gmail.com>
2022-07-06 17:26:53 +0200

    adapt the opening hours display in ShopDetails.jsx

--- frontend/src/components/ShopDetails.jsx
+++ frontend/src/components/ShopDetails.jsx
@@ -12,7 +12,7 @@ import fossier from "../assets/images/fossier.png";
 
 export default function ShopDetails() {
   const { id } = useParams();
-  const [shop, setShop] = useState([]);
+  const [shop, setShop] = useState();
 
   const getShop = () => {
     axios
@@ -21,49 +21,68 @@ export default function ShopDetails() {
           import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"
         }/shops/${id}`
       )
-      .then((response) => setShop(response.data));
+      .then((response) => response.data)
+      .then((data) => setShop(data));
   };
 
   useEffect(() => getShop, []);
 
   return (
     <section className="mb-[4.5rem]">
-      <h1 className="text-center my-6 text-3xl">{shop.name}</h1>
-      <img
-        src={fossier}
-        alt={`${shop.name} façade`}
-        className="my-5 border-2 border-black max-w-[85%] mx-auto rounded-lg"
-      />
-      <ul>
-        <li className="shedules">{shop.opening_hours}</li>
-      </ul>
-      <ul className="flex justify-between mx-4 my-6">
-        <li>
-          <FontAwesomeIcon icon={faEnvelope} className="contact-icons" /> :{" "}
-          {shop.email}
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faPhone} className="contact-icons" /> :{" "}
-          {shop.phone}
-        </li>
-      </ul>
-      <ul className="flex justify-evenly my-4">
-        <li className="link-icons">
-          <a href={shop.fb_page} target="blank">
-            <FontAwesomeIcon icon={faFacebook} />
-          </a>
-        </li>
-        <li className="link-icons">
-          <a href={shop.insta_page} target="blank">
-            <FontAwesomeIcon icon={faInstagram} />
-          </a>
-        </li>
-        <li className="link-icons">
-          <a href={shop.website} target="blank">
-            <FontAwesomeIcon icon={faGlobe} />
-          </a>
-        </li>
-      </ul>
+      {shop && (
+        <div>
+          <h1 className="text-center my-6 text-3xl">{shop.name}</h1>
+          <img
+            src={fossier}
+            alt={`${shop.name} façade`}
+            className="my-5 border-2 border-black max-w-[85%] mx-auto rounded-lg"
+          />
+
+          <ul>
+            <li className="shedules">Lundi : {shop.opening_hours.lundi}</li>
+            <li className="shedules">Mardi : {shop.opening_hours.mardi}</li>
+            <li className="shedules">
+              Mercredi : {shop.opening_hours.mercredi}
+            </li>
+            <li className="shedules">Mercredi : {shop.opening_hours.jeudi}</li>
+            <li className="shedules">
+              Vendredi : {shop.opening_hours.vendredi}
+            </li>
+            <li className="shedules">Samedi : {shop.opening_hours.samedi}</li>
+            <li className="shedules">
+              Dimanche : {shop.opening_hours.dimanche}
+            </li>
+          </ul>
+
+          <ul className="flex justify-between mx-4 my-6">
+            <li>
+              <FontAwesomeIcon icon={faEnvelope} className="contact-icons" /> :{" "}
+              {shop.email}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faPhone} className="contact-icons" /> :{" "}
+              {shop.phone}
+            </li>
+          </ul>
+          <ul className="flex justify-evenly my-4">
+            <li className="link-icons">
+              <a href={shop.fb_page} target="blank">
+                <FontAwesomeIcon icon={faFacebook} />
+              </a>
+            </li>
+            <li className="link-icons">
+              <a href={shop.insta_page} target="blank">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </li>
+            <li className="link-icons">
+              <a href={shop.website} target="blank">
+                <FontAwesomeIcon icon={faGlobe} />
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
     </section>
   );
 }
