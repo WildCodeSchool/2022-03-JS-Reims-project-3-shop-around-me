3a8fc5973c12101e898f4da487c505b2cbc3c4d4
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-07-13 10:30:55 +0200

    Change the structure of the page to harmonize the inferface with the rest

--- frontend/src/pages/MyAccount.jsx
+++ frontend/src/pages/MyAccount.jsx
@@ -13,33 +13,75 @@ import logo from "../assets/images/logo_alone.png";
 const accountMenu = [
   {
     id: 1,
-    icon: faPiggyBank,
-    title: "Historique cagnotte",
-    url: "/account/",
+    categorie: "Mon Shop Around",
+    titles: [
+      {
+        id: 1,
+        title: "Historique cagnotte",
+        url: "/account/info",
+        icon: faPiggyBank,
+      },
+    ],
   },
   {
     id: 2,
-    icon: faUser,
-    title: "Mes données personnelles",
-    url: "/account/mydata/",
+    categorie: "Gérer mon profil",
+    titles: [
+      {
+        id: 1,
+        title: "Mes données personnelles",
+        url: "/account/info",
+        icon: faUser,
+      },
+      {
+        id: 2,
+        title: "Mes adresses",
+        url: "/account/address",
+        icon: faUser,
+      },
+      {
+        id: 3,
+        title: "Paramètres du compte",
+        url: "/account/orders",
+        icon: faGears,
+      },
+    ],
   },
   {
     id: 3,
-    icon: faGears,
-    title: "Paramètres de l'application",
-    url: "/account/",
+    categorie: "Paramètre de l'application",
+    titles: [
+      {
+        id: 1,
+        title: "Préférence de communication",
+        url: "/account/info",
+        icon: faCircleInfo,
+      },
+      {
+        id: 2,
+        title: "Préférence de gestion de données",
+        url: "/account/info",
+        icon: faCircleInfo,
+      },
+    ],
   },
   {
     id: 4,
-    icon: faCircleInfo,
-    title: "Aide et contact",
-    url: "/account/",
-  },
-  {
-    id: 5,
-    icon: faUserXmark,
-    title: "Supprimer mes données",
-    url: "/account/",
+    categorie: "Information et assistance",
+    titles: [
+      {
+        id: 1,
+        title: "À propod de l'application",
+        url: "/account/info",
+        icon: faUserXmark,
+      },
+      {
+        id: 2,
+        title: "Aide et contact",
+        url: "/account/info",
+        icon: faUserXmark,
+      },
+    ],
   },
 ];
 
@@ -55,25 +97,31 @@ export default function MyAccount() {
   };
 
   return (
-    <main className="flex flex-col w-screen px-8 pt-8 pb-8 tracking-wide text-[#4F4E47]">
-      <img src={logo} alt="logo" className="max-w-[4rem] mr-2 mb-8" />
-      <p className=" text-2xl ">Bonjour {firstname} !</p>
-      <p className=" text-m mb-8">Votre compte.</p>
+    <main className="flex flex-col w-screen pb-8 tracking-wide text-[#4F4E47]">
+      <section className="px-8 pt-8">
+        <img src={logo} alt="logo" className="max-w-[4rem] mr-2 mb-8" />
+        <p className=" text-2xl">Bonjour {firstname} !</p>
+        <p className=" text-m mb-8">Votre compte.</p>
+      </section>
+      {accountMenu.map((menu) => (
+        <ul>
+          <p className="py-4 px-8 text-xl font-bold bg-[hsl(30,7%,71%)] cursor-pointer">
+            {menu.categorie}
+          </p>
+          {menu.titles.map((title) => (
+            <li className="ml-12 my-4 text-lg" key={title.id}>
+              <Link to={title.url}>
+                <FontAwesomeIcon icon={title.icon} className="mr-4 text-xl" />
+                {title.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ))}
 
-      <ul>
-        {accountMenu.map((menu) => (
-          <li key={menu.id} className="mr-4 text-lg list-none mb-4">
-            <Link to={menu.url} className="flex gap-8">
-              <FontAwesomeIcon icon={menu.icon} className="w-[24px] h-[24px]" />
-
-              {menu.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
       <button
         type="button"
-        className="text-m mt-8 py-3 border-solid border-2 border-red-600 text-red-600 font-bold rounded focus:outline-none focus:shadow-outline"
+        className="text-m mb-16 mt-4 py-3 border-solid border-2 border-red-600 text-red-600 font-bold rounded focus:outline-none focus:shadow-outline"
         onClick={handleLogout}
       >
         Se déconnecter
